556874532559cb6ade31716f403fee0aee088b1c
Emerson Jahn <emersonajahn@gmail.com>
2021-03-19 13:01:07 -0300

    Started the Minefield Component.

---
+++ src/components/MineField.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, StyleField } from 'react-native'
+import Field from './Field'
+
+export default props => {
+    const rows = props.board.map((rows, r) => {
+        const columns = row.map((field, c) => {
+            
+        })
+    })
+}
