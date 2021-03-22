c0b5ce870b6f768a755fbdba7fb859c3c84acea1
Emerson Jahn <emersonajahn@gmail.com>
2021-03-22 13:20:34 -0300

    Finished the Minefield Component.

--- src/components/MineField.js
+++ src/components/MineField.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { View, StyleField } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import Field from './Field'
 
 export default props => {
-    const rows = props.board.map((rows, r) => {
+    const rows = props.board.map((row, r) => {
         const columns = row.map((field, c) => {
-            
+            return <Field {...field} key={c}></Field>
         })
+        return <View key={r} style={{flexDirection: 'row'}}>{columns}</View>
     })
+    return <View style={styles.container}>{rows}</View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#EEE'
+    }
+})
