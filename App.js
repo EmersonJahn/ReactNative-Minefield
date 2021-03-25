10eaa376402e2608b33ff439cf8b878954daef46
Emerson Jahn <emersonajahn@gmail.com>
2021-03-25 12:51:34 -0300

    OpenField Implmented.

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Alert } from 'react-native';
 import params from './src/params';
 import MineField from './src/components/MineField'
-import { createMineBoard} from './src/functions'
+import { createMineBoard, cloneBoard, opendField, hadExplosion, wonGame, showMines } from './src/functions'
 
 
 export default class App extends Component {
@@ -22,8 +22,28 @@ export default class App extends Component {
     const cols = params.getColomnsAmount()
     const rows = params.getRowsAmount()
     return {
-      board: createMineBoard(rows, cols, this.minesAmount())
+      board: createMineBoard(rows, cols, this.minesAmount()),
+      won: false,
+      lost: false
     }
+  }
+
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    opendField(board, row, column)
+    const lost = hadExplosion(board)
+    const won = wonGame(board)
+
+    if (lost) {
+      showMines(board)
+      Alert.alert('Perdeeeu!!', 'Que buuurro!')
+    }
+
+    if (won) {
+      Alert.alert('Parabéns!!', 'Você venceu!')
+    }
+
+    this.setState({ board, lost, won })
   }
 
   render() {
@@ -40,7 +60,7 @@ export default class App extends Component {
         </Text>
 
         <View style={styles.board}>
-          <MineField board={this.state.board}></MineField>
+          <MineField board={this.state.board} onOpenField={this.onOpenField}></MineField>
         </View>
 
         {/* <Text>
